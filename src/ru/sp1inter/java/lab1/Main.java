cb9a7754ee26990c3beab0b4733d3aa27a708141
Sp1inter <splinter2905@yandex.ru>
2021-09-14 16:50:20 +0300

    Initial commit

---
+++ src/ru/sp1inter/java/lab1/Main.java
@@ -0,0 +1,19 @@
+package ru.sp1inter.java.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите какой-нибудь фрукт");
+        String fruit = scanner.nextLine();
+        System.out.println("вы ввели "+fruit);
+
+
+
+
+
+
+    }
+}
---
+++ src/ru/sp1inter/java/lab1/products/Product.java
@@ -0,0 +1,14 @@
+package ru.sp1inter.java.lab1.products;
+
+public class Product {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
