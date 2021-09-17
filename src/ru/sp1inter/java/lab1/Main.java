de4f66ec7efb38b943b36edcc73e0b605270b573
Sp1inter <splinter2905@yandex.ru>
2021-09-17 22:31:56 +0300

    сделал первое задание

--- src/ru/sp1inter/java/lab1/Main.java
+++ src/ru/sp1inter/java/lab1/Main.java
@@ -6,14 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("введите какой-нибудь фрукт");
+        System.out.println("Введите названия продуктов:");
+
         String fruit = scanner.nextLine();
-        System.out.println("вы ввели "+fruit);
 
-
-
-
+        String[] fruits = fruit.split(", ");
+        for (String split : fruits) {
+            switch (split) {
+                case "Яблоко":
+                case "Апельсин":
+                case "Арбуз":
+                case "Банан":
+                case "Мандарин":
+                    System.out.println(split+ " съеден!");
+                    break;
+                default:
+                    System.out.println(split + " - не предусмотренный продукт");
+            }
+        }
 
 
     }
 }
+/*Сделать консольную программу, чтобы писала
+"Введите названия продуктов:"
+Ты вводишь через запятую (выбор из этих продуктов "Яблоко",  "Апельсин",  "Банан", "Арбуз", "Мандарин"), например:
+"Яблоко, Апельсин, Арбуз, Машина"
+И в консоль выводится:
+"Яблоко съедено!
+Апельсин съедено!
+Арбуз съедено!
+Машина - не предусмотренный продукт"*/
